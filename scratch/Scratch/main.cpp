bff94cd8785368bfad76905c8bb37d0164400dc2
Jerome Barrun Pasion <jerome.b.pasion@gmail.com>
2024-05-12 17:47:46 +0200

    CPP Scratch
    More scratch

--- scratch/Scratch/main.cpp
+++ scratch/Scratch/main.cpp
@@ -3,6 +3,16 @@
 #include <string>
 using namespace std;
 
+template <class XYZ>
+XYZ sum (XYZ a, XYZ b){
+    XYZ sum;
+    sum = a + b;
+    return sum;
+}
+
+string concatenate (const string &first,const string &second){
+    return first+second;
+}
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -10,7 +20,25 @@ int main(int argc, char *argv[])
     //for testing C++ features
 
     cout << "Test" << endl;
+
+    string hello="Hello ";
+    string world="World";
+    cout << concatenate (hello,world);
+
+
+    int aInt = sum<int>(5,6);
+    float aFloat= sum<float>(5.1, 5.9);
+    cout << "Sum int: " << aInt << " float; " <<aFloat <<endl;
+
+
+
+
+
+
+
+
 //    a.exec();
 //    a.exit();
     return 0;
 }
+
