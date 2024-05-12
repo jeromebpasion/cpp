59227dd7afdf721e2228ae026921e7910c9ed059
Jerome Barrun Pasion <jerome.b.pasion@gmail.com>
2024-05-12 15:52:41 +0200

    CPP Scratch
    Add Scratch project for testing CPP things.

---
+++ scratch/Scratch/main.cpp
@@ -0,0 +1,16 @@
+#include <QCoreApplication>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    //for testing C++ features
+
+    cout << "Test" << endl;
+//    a.exec();
+//    a.exit();
+    return 0;
+}
