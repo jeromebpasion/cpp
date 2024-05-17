a35414bd0d4bd77dd7935d438255aeb40a8367ce
Jerome Barrun Pasion <jerome.b.pasion@gmail.com>
2024-05-17 23:45:12 +0200

    CPP Scratch:
    Updated with new experiments

--- scratch/Scratch/main.cpp
+++ scratch/Scratch/main.cpp
@@ -30,9 +30,24 @@ int main(int argc, char *argv[])
     float aFloat= sum<float>(5.1, 5.9);
     cout << "Sum int: " << aInt << " float; " <<aFloat <<endl;
 
+    int array[] = {3232, 231,55,03, 0x333};
+    cout << "array size " << sizeof(array)<<  '\n';
+    cout << "int size " << sizeof(int)<<  '\n';
+    for (int i = 0; i < 5; ++i){
+
+        cout << i << ' ' << array[i] << ' ' << &array[i] <<  '\n';
+    }
+
+    int myvar;
+    int * myptr;
+    *myptr = myvar;
+    cout << myvar << " " << myptr << " " << *myptr <<  '\n';
 
 
-
+    int * dynptr = new (nothrow) int [5];
+    if (dynptr == nullptr) {
+        cout << "Error: nullptr";
+    }
 
 
 
